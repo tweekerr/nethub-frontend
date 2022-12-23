dcf81b7991e4453457305389bc055fb2a7f34685
tweekerr <geektweeker@gmail.com>
2022-12-23 21:30:18 +0200

    Fix env types

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,18 +1,19 @@
 /// <reference types="vite/client" />
 
-
-import {ZodError} from "zod";
-
 export type ILanguage = 'uk' | 'en';
 
-declare module "zod" {
-  export interface ZodError {
-    formatErrors: () => { field: string, message: string }[]
-  }
+export interface TinyConfig {
+  plugins: string[];
+  key: string;
+  toolbar: string;
 }
 
-ZodError.prototype.formatErrors = function (): { field: string, message: string }[] {
-  return this.errors.map(e => {
-    return {field: e.path.toString(), message: e.message}
-  })
+declare global {
+  interface Window {
+    Telegram: {
+      Login: {
+        auth: (config: any, callback: (data: any) => void) => void;
+      }
+    };
+  }
 }
